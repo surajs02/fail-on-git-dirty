ca36ed88294ab5f9e7730cedfb2b4244cb61db37
surajs02 <suraj02@hotmail.co.uk>
2020-04-04 18:46:18 +0100

    Fix broken npm link for cli

--- failOnGitDirty.js
+++ failOnGitDirty.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+// The above shebang is needed for `npm link` or `bin` prop.
+
 const { execSync } = require('child_process');
 
 const isDirty = execSync('git status --porcelain').toString().length > 0;
