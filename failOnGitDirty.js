cbc6c66ca21612c79375cdc6b80a6fa9cf2f0bbe
surajs02 <suraj02@hotmail.co.uk>
2020-04-04 18:27:43 +0100

    Add bin prop so available in cli

--- failOnGitDirty.js
+++ failOnGitDirty.js
@@ -1,5 +1,4 @@
 const { execSync } = require('child_process');
 
 const isDirty = execSync('git status --porcelain').toString().length > 0;
-console.log(isDirty)
 if (isDirty) throw new Error('Failed as git status is dirty!');
